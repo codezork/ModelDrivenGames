6f0fa6172d3802409e9d0fa4168553b658d16d30
JR <joerg.riegel@web.de>
2018-04-18 23:25:53 +0200

    merged development

---
+++ runtime-Game/GameModel/resources/tileset_gutter.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset_gutter" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="924" columns="33">
+ <image source="../../../../../../libgdx/SuperMario/android/assets/tileset_gutter.png" trans="ff00ff" width="594" height="504"/>
+</tileset>
